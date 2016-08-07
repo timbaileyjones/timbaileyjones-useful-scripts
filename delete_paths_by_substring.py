f628fda271a267875271e0839fcf043340304d93
Tim Bailey-Jones <tim@bailey-jones.com>
2016-08-07 12:43:29 -0400

    first semi-full implementation of delete_paths_by_substring.py

--- delete_paths_by_substring.py
+++ delete_paths_by_substring.py
@@ -1,7 +1,54 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 
+# vim: number:ts=4:shiftwidth=4:expandtab:nows
+    
 import re
 import os
+import sys
+import shutil
 
+print "sys.argv", sys.argv
 
+if len(sys.argv) < 3:
+    print "Usage: delete_paths_by_substring.py %s %s" % ("filename-list", "regex-to-delete")
+    sys.exit(1)
 
+regex = re.compile(sys.argv[2])
+
+filenames = []
+with open(sys.argv[1]) as f:
+    filenames = f.read().splitlines()
+
+print "filename length", len(filenames)
+print "regex", regex
+
+matches = []
+nonmatches = []
+for f in filenames:
+    if regex.search(f):
+        print "matched ", f
+        matches.append(f)
+    else:
+        nonmatches.append(f)
+
+print "%d filenames, %d matches" % (len(filenames), len(matches))
+
+del_succ = 0
+del_fail = 0
+for f in matches:
+    try:
+        os.unlink(f)
+        del_succ = del_succ + 1
+    except:
+        del_fail = del_fail + 1
+print
+
+with open("delete-by-regex.log", "a") as f:
+    f.write( "%s %d %d\n" % (sys.argv[2], del_succ, del_fail))
+
+with open(".tmp", "w") as f:
+    for line in nonmatches:
+        f.write(line)
+        f.write('\n')
+
+shutil.move(".tmp", sys.argv[1])
