ea51c6812cbb0d859d053975097bb088bf3c995d
Tim Bailey-Jones <tim@bailey-jones.com>
2025-09-09 21:11:48 -0400

    updates to my battery monitoring script

--- MAC/announce-battery-level-changes.sh
+++ MAC/announce-battery-level-changes.sh
@@ -11,17 +11,20 @@ prev=$(batt)    # grab current battery level as previous, so that it doesn't ann
 while :
 do
   pct=$(batt)
+  if [ $prev -lt 100 -a $pct -eq 100 ];then
+    echo say battery is fully charged at 100%
+  fi
   if [ $pct -lt $prev ];then
     echo $(date) $(pmset -g batt | grep InternalBattery )
-    if [ $pct -le 30 ];then
+    if [ $pct -le 10 ];then
       say battery fell from $prev percent to $pct
     fi
   elif [ $pct -gt $prev ];then
     echo $(date) $(pmset -g batt | grep InternalBattery )
-    if [ $pct -le 30 ];then
+    if [ $pct -le 10 ];then
       say battery at $pct %
     fi
   fi
   prev=$pct
-  sleep 10
+  sleep 2
 done
